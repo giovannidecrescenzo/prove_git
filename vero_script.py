42b4c117d042f02da62369f978cc63448a3190e0
Giovanni De Crescenzo <giovannidecrescenzo@MacBook-Air-di-Giovanni.local>
2023-02-27 18:19:12 +0100

    corrected another fucking time

--- vero_script.py
+++ vero_script.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 
 x = np.random.normal(0,3.1, 100000)
 y = np.random.normal(10,2.1, 100000)
-
-fig, ax = plt.subplot()
-ax.hist(x,bins = 1000, alpha = 0.8, label = "SSSSEEEEEE", color = "skyblue")
-ax.hist(y,bins = 1000, alpha = 0.8, label = "SSSSEEEEEE", color = "red")
-fig.savefig("megaplot.png")
+plt.hist(x,bins = 1000, alpha = 0.8, label = "SSSSEEEEEE", color = "skyblue")
+plt.hist(y,bins = 1000, alpha = 0.8, label = "SSSSEEEEEE", color = "red")
+plt.savefig("megaplot.png")
 
