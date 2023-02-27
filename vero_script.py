04b9a56db0d66e938c07a4fda4211228338b0566
Giovanni De Crescenzo <giovannidecrescenzo@MacBook-Air-di-Giovanni.local>
2023-02-27 18:16:44 +0100

    fatto le modifiche

--- vero_script.py
+++ vero_script.py
@@ -9,7 +9,5 @@ y = np.random.normal(10,2.1, 100000)
 fig, ax = plt.subplot()
 ax.hist(x,bins = 1000, alpha = 0.8, label = "SSSSEEEEEE", color = "skyblue")
 ax.hist(y,bins = 1000, alpha = 0.8, label = "SSSSEEEEEE", color = "red")
-plt.label()
-plt.show()
-plt.savefig("megaplot.png")
+fig.savefig("megaplot.png")
 
